0aac8cafc5e57c0723d22e67322be0cf66855e7a
David <d.d.faruntsev@st.hanze.nl>
2025-01-14 14:30:45 +0100

    Can view all packages

--- lib/controllers/packagescontroller.dart
+++ lib/controllers/packagescontroller.dart
@@ -1,3 +1,35 @@
-class Packagescontroller {
-  
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class PackagesController {
+  final String baseUrl;
+  final storage = const FlutterSecureStorage();
+
+  PackagesController({required this.baseUrl});
+
+  Future<List<dynamic>> fetchPackages() async {
+    String? token = await storage.read(key: 'jwt');
+    if (token == null) {
+      throw Exception("JWT token niet gevonden. Log eerst in.");
+    }
+
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+
+    
+    var response = await http.get(
+      Uri.parse('$baseUrl/api/delivery-packages'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      
+      return jsonDecode(response.body);
+    } else {
+      throw Exception("Kan pakketten niet ophalen: ${response.statusCode}");
+    }
+  }
 }
