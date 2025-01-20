40a6c6d255ee10f34bcd01cd65e35e3c71e152d3
Joeke <joeke@posteo.net>
2025-01-20 13:08:55 +0100

    InsuranceView als StatefulWidget die alleen een id meekrijgt

--- lib/views/InsuranceView.dart
+++ lib/views/InsuranceView.dart
@@ -1,41 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:software_startup/classes/Package.dart';
 import 'package:software_startup/common/CustomStyles.dart';
+import 'package:software_startup/controllers/packagescontroller.dart';
 
-/* Deze view moet 'aangeroepen' worden vanuit een knop op een pakket in
-PackagesView. Dan kan het benodigde Package-object meegegeven worden. */
 class InsuranceView extends StatefulWidget {
-  const InsuranceView({super.key, required this.package});
-  final Package package;
+  //final InsuranceController controller = InsuranceController();
+  InsuranceView({super.key, required this.id});
+  final int id;
 
   @override
-  State<InsuranceView> createState() => _InsuranceViewState();
+  _InsuranceViewState createState() => _InsuranceViewState();
 }
 
 class _InsuranceViewState extends State<InsuranceView> {
+  final PackagesController controller =
+      PackagesController(baseUrl: 'http://10.0.2.2:8080');
+
   @override
   Widget build(BuildContext context) {
+    Future<Package>? package = controller.getPackageById(widget.id);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Verzekeren"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CustomStyles.willemRijdtPackageCard(widget.package.id,
-                widget.package.status, widget.package.weightGrams,
-                packageDesc: widget.package.description),
-            //PackageCard(package: widget.package),
-            Text(
-                "Het verzekeren van dit pakket kost: ${widget.package.shippingPrice / 4}"),
-            TextButton(
-                onPressed: () => {},
-                child: const Text("Verzekeren")) // TODO: onPressed
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Verzekeren"),
         ),
-      ),
-    );
+        body: Center(
+          child: FutureBuilder(
+              future: package,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomStyles.willemRijdtPackageCard(snapshot.data!.id,
+                            snapshot.data!.status, snapshot.data!.weightGrams,
+                            packageDesc: snapshot.data!.description,
+                            button: TextButton(
+                                onPressed: () => {},
+                                child: const Text('Verzekeren'))),
+                        Text(
+                            'Het verzekeren van dit pakket kost: ${snapshot.data!.shippingPrice / 4}')
+                      ]);
+                } else if (snapshot.hasError) {
+                  return Text(
+                      'Error getting data: ${snapshot.error.toString()}');
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              }),
+        ));
   }
 }
